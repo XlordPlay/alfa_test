02a161bda3efb1f451a7d26b56d14c8b8c8f4ad8
XlordPlay <s.kirill150282@mail.ru>
2025-02-18 18:27:03 +0300

    add task1

---
+++ task1/src/add_data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+file_path = "/home/xlordplay/alfa_test_tasks/alfa_test/data/Input data/data_prep.tab"  
+
+df = pd.read_csv(file_path, sep="\t")
+df.sort_values(by=["CLIENT_ID", "SEGM_DATE"], inplace=True)
+
+
+num_columns = [col for col in df.columns if col not in ["CLIENT_ID", "SEGM_DATE"]]
+
+
+original_columns = df.columns.tolist()
+new_columns = []
+
+for col in original_columns:
+    new_columns.append(col)
+    if col in num_columns:
+        df[f"{col}_1"] = df.groupby("CLIENT_ID")[col].shift(1)  # Предыдущий месяц
+        df[f"{col}_2"] = df.groupby("CLIENT_ID")[col].shift(2)  # Предпредыдущий месяц
+        new_columns.extend([f"{col}_1", f"{col}_2"])
+
+df = df[new_columns]
+
+output_file = "data/data_prep_transforme.csv"  
+df.to_csv(output_file, index=False)
+
+print(f"File saved successfully: {output_file}")
