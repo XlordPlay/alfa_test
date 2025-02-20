835938ba905a4bd710cbe7487c1f67b92e64c34e
XlordPlay <s.kirill150282@mail.ru>
2025-02-20 17:24:54 +0300

    add minimal api

---
+++ task2/api/model.py
@@ -0,0 +1,85 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import onnxruntime as ort
+import numpy as np
+import json
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+import os
+from fastapi.responses import FileResponse
+
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+features_path = os.path.join(root_dir, "src", "onnx", "selected_features.json")
+html_file_path_for_mount = os.path.join(root_dir, "src", "front_end")
+MODEL_PATH = os.path.join(root_dir, "src", "onnx", "RandomForest.onnx")
+html_file_path = os.path.join(root_dir, "src", "front_end", "content.html") 
+
+
+
+with open(features_path, "r") as f:
+    FEATURES = json.load(f)
+
+
+
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.mount(html_file_path_for_mount, StaticFiles(directory=html_file_path_for_mount), name="static")
+
+class InputData(BaseModel):
+    features: dict
+
+@app.post("/predict")
+def predict(data: InputData, threshold: float = 0.01):  # Параметр порога по умолчанию 0.5
+    try:
+        # Логируем входные данные
+        print("Received features:", data.features)
+
+        # Проверяем, что все необходимые признаки переданы
+        if not all(feature in data.features for feature in FEATURES):
+            raise HTTPException(status_code=400, detail="Некорректные входные данные")
+        
+        # Преобразуем входные данные в массив
+        X = np.array([[data.features[f] for f in FEATURES]], dtype=np.float32)
+        session = ort.InferenceSession(MODEL_PATH)
+        input_name = session.get_inputs()[0].name
+        # Логируем массив для предсказания
+        print("Input array for prediction:", X)
+
+        # Предсказание
+        pred = session.run(None, {input_name: X})[0]
+        if pred == 0:
+            print("Warning: Predicted probability is 0.")
+        # Логируем предсказание
+        print("Raw prediction result:", pred[0])
+
+        # Применяем порог для классификации
+        prediction_class = 1 if pred[0] >= threshold else 0
+        
+        return {"prediction": prediction_class}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+
+
+
+with open(html_file_path, 'r') as file:
+    html_content = file.read()
+
+# Теперь html_content содержит HTML из файла
+@app.get("/")
+def root():
+    return FileResponse(html_file_path)
--- task3/__init__.py
+++ task3/__init__.py
@@ -1,24 +1,3 @@
-import pandas as pd
-import numpy as np 
-import seaborn as sns
-import matplotlib.pyplot as plt
-data = pd.read_csv("/home/xlordplay/alfa_test_tasks/alfa_test/data/Input data/abt_em.tab",sep="\t")
-
-categorical_feature = data.select_dtypes(include="object")
-binary_arrear_indicator = {"N" : 0 , "Y" : 1}
-gender_indicator = {"M" : 0 , "F" : 1, "U" : 2}
-chq_acc_ind_indicator = {"N" : 0 ,"Y" : 1}
-title_indicator = {"MR": 0 , "MRS" : 1 , "UNKNOWN" : 2, "MS": 3, "AC" : 4 , "MASTER" : 5, "REL" : 6,"MISS": 7 }
-
-indicator_dict = {
-    "arrear_ind": binary_arrear_indicator,
-    "chq_acc_ind": chq_acc_ind_indicator,
-    "gender": gender_indicator,
-    "title": title_indicator,
-}
-
-# Замена значений в DataFrame
-for col in categorical_feature:
-    if col in data.columns:  # Проверяем, существует ли столбец
-        data[col] = data[col].map(indicator_dict[col])
-data.head()
+import numpy as np
+print(np.__file__)  # Где установлен numpy
+print(np.__version__)  # Какая версия используется
